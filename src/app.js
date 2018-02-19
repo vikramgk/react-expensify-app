ff73c4f699c9efd1d7e1da46db542411f02f9ff5
vikram <gkvikram95@gmail.com>
2018-02-19 21:07:52 +1100

    boiler plate

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// remember to look for ES6 importing in docs
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<p>This is boiler plate</p>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// import path.join from node
+const path = require('path');
+console.log(path.join(__dirname, 'public'));
+
+// entry -> output location
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]            
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
