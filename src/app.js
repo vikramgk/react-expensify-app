578d2a63dc7bf756d16cef2e5f56b2a35e7f0478
vikram <gkvikram95@gmail.com>
2018-02-19 22:12:10 +1100

    added router page and 404 page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,50 @@
 // remember to look for ES6 importing in docs
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
-ReactDOM.render(<p>This is boiler plate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component.
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my expense component.
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit component.
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help component.
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
